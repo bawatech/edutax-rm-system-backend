a12b3984e1e161fc01d070f9f439e5bb9f0086bc
swaran_singh <deepnirmaan8@gmail.com>
2024-02-26 15:53:56 +0530

    route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, signUp, verifyEmail, forgotPassword, newPassword, updatePassword, logout } from "../contollers/auth.controller";
+import { login, signUp, verifyEmail, forgotPassword, newPassword, updatePassword, logout, verifyLogin } from "../contollers/auth.controller";
 import { clientAuth } from "../middlewares/clientAuth";
 const router = Router();
 
@@ -9,6 +9,7 @@ router.post("/sign-up", signUp);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
+router.post("/verify-login", verifyLogin);
 router.post("/forgot-password", forgotPassword);
 router.post("/new-password", newPassword);
 router.put("/update-password", clientAuth, updatePassword);
